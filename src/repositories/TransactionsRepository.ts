e1a9ecdb4d8ea87f0bbff73ad1c713f645bb4468
Camael Ventura <camael.ventura@gmail.com>
2021-01-07 01:53:18 -0300

    Soluçao

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(transactions => transactions.type === 'income')
+      .reduce((sumIncome, transaction) => sumIncome + transaction.value, 0);
+    const outcome = this.transactions
+      .filter(transactions => transactions.type === 'outcome')
+      .reduce((sumIncome, transaction) => sumIncome + transaction.value, 0);
+
+    const total = income - outcome;
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const balance = this.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw Error('Error. Your outcome is bigger them income');
+    }
+
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
